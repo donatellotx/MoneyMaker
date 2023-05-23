9f66e0f92a61e55a5e61d728a3e325bbcd2b0022
Donte Mayfield <donatello409@hotmail.com>
2023-05-23 17:14:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MoneyMaker
+    {
+        class MainClass
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Money Maker!");
+
+                Console.WriteLine("Enter an amount to calculate number of coins used");
+                string userInputAmount = Console.ReadLine();
+                double inputAmount = Convert.ToDouble(userInputAmount);
+                Console.WriteLine($"{inputAmount} cents is equal to...");
+
+                double goldValue = 10;
+                double silverValue = 5;
+
+                double goldCoins = Math.Floor(inputAmount / goldValue);
+                double remainder = (inputAmount % goldValue);
+
+                double silverCoins = Math.Floor(remainder / silverValue);
+                remainder = remainder % silverValue;
+
+                Console.WriteLine($" Gold coins: {goldCoins} \n Silver coins: {silverCoins} \n Bronze coins: {remainder}");
+
+
+            }
+        }
+
+}
+    
